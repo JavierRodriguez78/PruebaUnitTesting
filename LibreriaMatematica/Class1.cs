163ab0b4b0bfef0eeff22ad9e8a9f506a1e34dae
xavi <xavi@geekshubs.com>
2017-11-26 17:44:28 +0100

    Agregar archivos de proyecto.

---
+++ LibreriaMatematica/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaMatematica
+{
+    public class LibreriaMatematica
+    {
+
+        public static bool EsPar(int numero)
+        {
+            return Convert.ToBoolean((numero % 2 == 0 ? true : false));
+        }
+
+
+        public static bool EsPrimo(int numero)
+        {
+            int sumatorio = 0;
+            for (int contador = 1; contador < numero + 1; contador++)
+            {
+                if (contador % 2 == 0)
+                {
+                    sumatorio++;
+                }
+            }
+
+                if (sumatorio != 2)
+                {
+                    return false;
+                }else
+                {
+                    return true;
+                }
+
+            
+        }           
+    }
+}
---
+++ PruebasUnitarias/PruebasUnitarias.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using LibreriaMatematica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasUnitarias
+{
+    [TestFixture]
+    public class PruebasUnitarias
+    {
+        [Test]
+        public void PruebaNumeroPar()
+        {
+            //Arrange
+           int numero = 2;
+
+            //Act
+            bool resultado = LibreriaMatematica.LibreriaMatematica.EsPar(numero);
+
+            //Assert
+            bool expected = true;
+            Assert.AreEqual(expected, resultado);
+
+        }
+        [Test]
+        public void PruebaNumeroPrimo()
+        {
+
+            //Arrange
+            int numero = 5;
+
+            //Act
+            bool resultado = LibreriaMatematica.LibreriaMatematica.EsPrimo(numero);
+
+            //Asert
+            bool expected = true;
+            Assert.AreEqual(expected, resultado);
+        }
+    }
+}
